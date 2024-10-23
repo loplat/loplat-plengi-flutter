1d55f1b1f6b9754214ee110c755a8392d862c26b
rymin <rymin@ryminui-MacBookPro.local>
2024-10-23 12:17:49 +0900

    fix: example update (ios notification)

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -2,28 +2,48 @@ import Flutter
 import UIKit
 import MiniPlengi
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      GeneratedPluginRegistrant.register(with: self)
-      // ********** 중간 생략 ********** //
-      Plengi.initialize(clientID: "loplat", clientSecret: "loplatsecret")
-      
-      if Plengi.setDelegate(self) == .SUCCESS {
-          // setDelegate 등록 성공
-      } else {
-          // setDelegate 등록 실패
-      }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        // ********** 중간 생략 ********** //
+        Plengi.initialize(clientID: "loplat", clientSecret: "loplatsecret")
+        
+        if Plengi.setDelegate(self) == .SUCCESS {
+            // setDelegate 등록 성공
+        } else {
+            // setDelegate 등록 실패
+        }
+        if #available(iOS  10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                          didReceive response: UNNotificationResponse,
+                                          withCompletionHandler completionHandler: @escaping () -> Void) {
+        _ = Plengi.processLoplatAdvertisement(center,
+                                              didReceive: response,
+                                              withCompletionHandler: completionHandler)
+        completionHandler()
+    }
+    
+    @available(iOS 10.0, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         willPresent notification: UNNotification,
+                                         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 }
 
 extension AppDelegate: PlaceDelegate {
     func plengiAlert(_ message: String) {
-//        placeEventLocalNotification(place: message)
+        //        placeEventLocalNotification(place: message)
     }
     
     private func placeEventType(_ event: PlaceEvent) -> String {
@@ -43,7 +63,7 @@ extension AppDelegate: PlaceDelegate {
         if plengiResponse.echoCode != nil {
             // 고객사에서 설정한 echoCode
         }
-
+        
         if plengiResponse.result == .SUCCESS {
             if plengiResponse.place != nil {
                 if plengiResponse.placeEvent == .ENTER {
@@ -54,20 +74,20 @@ extension AppDelegate: PlaceDelegate {
                     // PlaceEvent가 NEARBY 일 경우, NEARBY 로 인식된 장소 정보가 넘어옴
                 }
             }
-
+            
             if plengiResponse.advertisement != nil {
                 // loplat X 광고 정보가 있을 때
                 // 기본으로 Plengi SDK에서 광고이벤트를 직접 알림으로 처리합니다.
                 // 하지만 설정값에 따라 광고이벤트를 직접 처리할 경우 해당 객체를 사용합니다.
             }
-      } else {
-        /* 여기서부터는 오류인 경우입니다 */
-        // plengiResponse.errorReason 에 위치 인식 실패 / 오류 이유가 포함됨
-
-        // FAIL : 위치 인식 실패
-        // NETWORK_FAIL : 네트워크 오류
-        // ERROR_CLOUD_ACCESS : 클라이언트 ID/PW가 틀렸거나 인증되지 않은 사용자가 요청했을 때
-        // Location Acquisition Fail : plengiResponse.location에서 위경도 값만 있는 경우
-      }
+        } else {
+            /* 여기서부터는 오류인 경우입니다 */
+            // plengiResponse.errorReason 에 위치 인식 실패 / 오류 이유가 포함됨
+            
+            // FAIL : 위치 인식 실패
+            // NETWORK_FAIL : 네트워크 오류
+            // ERROR_CLOUD_ACCESS : 클라이언트 ID/PW가 틀렸거나 인증되지 않은 사용자가 요청했을 때
+            // Location Acquisition Fail : plengiResponse.location에서 위경도 값만 있는 경우
+        }
     }
 }
